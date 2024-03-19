36550ab620d3fd1253214fe81a883294a13c9081
jonmejia <jonathan@dataminded.net>
2024-03-19 11:43:01 -0400

    fix: changed define from cd to leader-cd

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -19,7 +19,7 @@ return {
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({})
 
-			vim.keymap.set("n", "cd", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>cd", vim.lsp.buf.hover, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
